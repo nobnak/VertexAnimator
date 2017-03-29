d48d5c1bca735ab6c8eabe105fad3269f3484b91
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-03-29 15:58:38 +0900

    Unity 5.4 / 5.5 両対応

--- Editor/CreateAnimationTexture.cs
+++ Editor/CreateAnimationTexture.cs
@@ -95,22 +95,42 @@ namespace VertexAnimater {
 		}
 
 		static Texture2D Save (Texture2D tex, string pngPath) {
-			File.WriteAllBytes (pngPath, tex.EncodeToPNG ());
-			AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
-			var pngImporter = (TextureImporter)AssetImporter.GetAtPath (pngPath);
-			var pngSettings = new TextureImporterSettings ();
-			pngImporter.ReadTextureSettings (pngSettings);
-			pngSettings.filterMode = ANIM_TEX_FILTER;
-			pngSettings.mipmapEnabled = false;
-			pngSettings.sRGBTexture = false;
-			pngSettings.wrapMode = TextureWrapMode.Clamp;
-			pngImporter.SetTextureSettings (pngSettings);
-			var platformSettings = pngImporter.GetDefaultPlatformTextureSettings ();
-			platformSettings.format = TextureImporterFormat.RGB24;
-			platformSettings.maxTextureSize = Mathf.Max (platformSettings.maxTextureSize, Mathf.Max (tex.width, tex.height));
-			pngImporter.SetPlatformTextureSettings (platformSettings);
-			AssetDatabase.WriteImportSettingsIfDirty (pngPath);
-			AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
+            #if UNITY_5_5_OR_NEWER
+            File.WriteAllBytes (pngPath, tex.EncodeToPNG ());
+            AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
+            var pngImporter = (TextureImporter)AssetImporter.GetAtPath (pngPath);
+            var pngSettings = new TextureImporterSettings ();
+            pngImporter.ReadTextureSettings (pngSettings);
+            pngSettings.filterMode = ANIM_TEX_FILTER;
+            pngSettings.mipmapEnabled = false;
+            pngSettings.sRGBTexture = false;
+            pngSettings.wrapMode = TextureWrapMode.Clamp;
+            pngImporter.SetTextureSettings (pngSettings);
+            var platformSettings = pngImporter.GetDefaultPlatformTextureSettings ();
+            platformSettings.format = TextureImporterFormat.RGB24;
+            platformSettings.maxTextureSize = Mathf.Max (platformSettings.maxTextureSize, Mathf.Max (tex.width, tex.height));
+            pngImporter.SetPlatformTextureSettings (platformSettings);
+            AssetDatabase.WriteImportSettingsIfDirty (pngPath);
+            AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
+
+            #else
+            File.WriteAllBytes (pngPath, tex.EncodeToPNG ());
+            AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
+            var pngImporter = (TextureImporter)AssetImporter.GetAtPath (pngPath);
+            var pngSettings = new TextureImporterSettings ();
+            pngImporter.ReadTextureSettings (pngSettings);
+            pngSettings.filterMode = ANIM_TEX_FILTER;
+            pngSettings.mipmapEnabled = false;
+            pngSettings.linearTexture = true;
+            pngSettings.wrapMode = TextureWrapMode.Clamp;
+            pngImporter.SetTextureSettings (pngSettings);
+            pngImporter.textureFormat = TextureImporterFormat.RGB24;
+            pngImporter.maxTextureSize = Mathf.Max (pngImporter.maxTextureSize, Mathf.Max (tex.width, tex.height));
+            pngImporter.SaveAndReimport();
+            //AssetDatabase.WriteImportSettingsIfDirty (pngPath);
+            //AssetDatabase.ImportAsset (pngPath, ImportAssetOptions.ForceUpdate);
+            #endif
+
 			return (Texture2D)AssetDatabase.LoadAssetAtPath (pngPath, typeof(Texture2D));
 		}
         static bool ContainsAllFlags(CreationModeFlags flags, CreationModeFlags contains) {
