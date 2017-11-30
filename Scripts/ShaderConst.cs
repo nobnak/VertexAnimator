88425955b4daba04f41ccd0d2932745dc0c650c5
DAVIS Elliott <davis@team-lab.com>
2017-11-30 19:41:40 +0900

    Adding SHADER_ANIM_TEX_TIME shader constant define to allow c# code to more easily access animation playback time in shader.

--- Scripts/ShaderConst.cs
+++ Scripts/ShaderConst.cs
@@ -6,6 +6,7 @@ namespace VertexAnimater {
     public static class ShaderConst {
         public const string SHADER_NAME = "VertexAnim/OneTime";
 		public const string SHADER_ANIM_TEX = "_AnimTex";
+		public const string SHADER_ANIM_TEX_TIME = "_AnimTex_T";
         public const string SHADER_SCALE = "_AnimTex_Scale";
         public const string SHADER_OFFSET = "_AnimTex_Offset";
         public const string SHADER_ANIM_END = "_AnimTex_AnimEnd";
